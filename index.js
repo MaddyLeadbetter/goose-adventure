79876e9d8201d89e9a5f57615d3d2a8b9455b76d
Maddy <maddyleadbetter@rogers.com>
2020-05-16 10:34:05 -0400

    Update index with a few instructions

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var playGame = confirm('Do you want to play? (yes/no)');
+// Basics
+//     alert('string); <= no options, just informing you
+//     confirm('string'); <= has yes/no confirmation
+//     prompt('string'); <= has text box for user to fill
+
+const textArr = ['string1', 'string2'];
+
+const playGame = confirm('Do you want to play? (yes/no)');
 
 if (playGame) {
 //next prompt
