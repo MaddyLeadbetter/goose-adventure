56cbb0cde42523b2db2750c73cbb4762f5d8afad
Yangzi Guo <yangzi1guo@gmail.com>
2020-05-16 12:42:04 -0400

    Updates

---
+++ precache-manifest.3635fa1abb41c18a202fe2e2b48bd473.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87b1fc7241b6fbaab62046a171165c8",
+    "url": "/goose-adventure/index.html"
+  },
+  {
+    "revision": "7d267616f8f51b739f8d",
+    "url": "/goose-adventure/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c1a9022911ebeb3c3dae",
+    "url": "/goose-adventure/static/js/2.b6e20e8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goose-adventure/static/js/2.b6e20e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d267616f8f51b739f8d",
+    "url": "/goose-adventure/static/js/main.6f894d37.chunk.js"
+  },
+  {
+    "revision": "46bf005cbf97d6944c4f",
+    "url": "/goose-adventure/static/js/runtime-main.12e5e3ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goose-adventure/static/media/logo.5d5d9eef.svg"
+  }
+]);
