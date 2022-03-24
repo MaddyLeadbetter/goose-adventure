d733e56d6ff22ebe3abdf92f3c3ff9c0cf3c3ecc
unknown <maddyleadbetter@rogers.com>
2022-03-24 18:54:26 -0400

    Updates

---
+++ precache-manifest.9e099d9a336e5ffe5579d58676d21508.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01de16bf3a95d4cc7829b0c552b88f5e",
+    "url": "/goose-adventure/index.html"
+  },
+  {
+    "revision": "b8968864cfa1373562d8",
+    "url": "/goose-adventure/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6ca714fa0d06d2c61b27",
+    "url": "/goose-adventure/static/css/main.8207a860.chunk.css"
+  },
+  {
+    "revision": "b8968864cfa1373562d8",
+    "url": "/goose-adventure/static/js/2.5613b06d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goose-adventure/static/js/2.5613b06d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca714fa0d06d2c61b27",
+    "url": "/goose-adventure/static/js/main.95b97823.chunk.js"
+  },
+  {
+    "revision": "46bf005cbf97d6944c4f",
+    "url": "/goose-adventure/static/js/runtime-main.12e5e3ed.js"
+  },
+  {
+    "revision": "3cb9e0e5eaa9ab4c320153cf7f1d5c01",
+    "url": "/goose-adventure/static/media/Goose banner.3cb9e0e5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/maddyleadbetter.github.io/goose-adventure/precache-manifest.f3b049e339dc1db6e492c3e3985f48a7.js"
+  "/goose-adventure/precache-manifest.9e099d9a336e5ffe5579d58676d21508.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/maddyleadbetter.github.io/goose-adventure/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goose-adventure/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
